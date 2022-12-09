24fcf9bc8ffbd2209aee5303ef29056a10bcef08
Ben Kogan <brk57@cornell.edu>
2022-12-09 03:36:34 -0500

    day 9

---
+++ 9/1.py
@@ -0,0 +1,24 @@
+# ans = 6087
+
+with open("i1.txt") as f:
+    moves = [line.strip() for line in f.readlines()]
+
+dir_map = {"R": (1, 0), "L": (-1, 0), "D": (0, -1), "U": (0, 1)}
+
+visited = set([(0, 0)])
+head_pos = (0, 0)
+tail_pos = (0, 0)
+for move in moves:
+    dir, steps = move.split()
+    dx, dy = dir_map[dir]
+    for _ in range(int(steps)):
+        head_pos = head_pos[0] + dx, head_pos[1] + dy
+
+        match (abs(head_pos[0] - tail_pos[0]), abs(head_pos[1] - tail_pos[1])):
+            case (2, _) | (_, 2):
+                # move to previous head location
+                tail_pos = head_pos[0] - dx, head_pos[1] - dy
+                visited.add(tail_pos)
+
+
+print(len(visited))
---
+++ 9/2.py
@@ -0,0 +1,30 @@
+# ans = 2493
+
+KNOTS = 10
+
+with open("i2.txt") as f:
+    moves = [line.strip() for line in f.readlines()]
+
+dir_map = {"R": (1, 0), "L": (-1, 0), "D": (0, -1), "U": (0, 1)}
+
+
+visited = set([(0, 0)])
+positions = [(0, 0)] * KNOTS
+for move in moves:
+    dir, steps = move.split()
+    dx, dy = dir_map[dir]
+    for _ in range(int(steps)):
+        positions[0] = positions[0][0] + dx, positions[0][1] + dy
+        for i in range(1, KNOTS):
+            px, py = positions[i - 1]
+            cx, cy = positions[i]
+            match (abs(px - cx), abs(py - cy)):
+                case (2, _) | (_, 2):
+                    next_x = (px + cx) / 2 if abs(px - cx) == 2 else px
+                    next_y = (py + cy) / 2 if abs(py - cy) == 2 else py
+                    positions[i] = next_x, next_y
+
+        visited.add(positions[-1])
+
+
+print(len(visited))
