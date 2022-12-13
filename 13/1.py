4b8a9a14881374b92796b19f891ad82f55dd054c
Ben Kogan <brk57@cornell.edu>
2022-12-13 03:41:08 -0500

    day 13

---
+++ 13/1.py
@@ -0,0 +1,36 @@
+# ans = 5503
+import json
+
+with open("i1.txt") as f:
+    pairs = f.read().strip().split("\n\n")
+
+decoded = []
+for pair in pairs:
+    x, y = pair.split("\n")
+    decoded.append((json.loads(x), json.loads(y)))
+
+# True if the pair is in the right order, None if order unknown
+def comp_pair(left, right):
+    match left, right:
+        case int(), int():
+            return None if left == right else left < right
+        case int(), list():
+            left = [left]
+        case list(), int():
+            right = [right]
+    if left == [] or right == []:
+        return None if len(left) == len(right) else len(left) < len(right)
+
+    result = comp_pair(left[0], right[0])
+    if result != None:
+        return result
+
+    return comp_pair(left[1:], right[1:])
+
+
+total = 0
+for i, (left, right) in enumerate(decoded):
+    if comp_pair(left, right):
+        total += i + 1
+
+print(total)
---
+++ 13/2.py
@@ -0,0 +1,35 @@
+# ans = 20952
+import json
+from functools import cmp_to_key
+
+with open("i2.txt") as f:
+    pairs = f.read().strip().split("\n\n")
+
+decoded = [[[2]], [[6]]]
+for pair in pairs:
+    x, y = pair.split("\n")
+    decoded += [json.loads(x), json.loads(y)]
+
+# negative if pair in right order, positive if wrong order, 0 if equal
+def comp_pair(left, right):
+    match left, right:
+        case int(), int():
+            return 0 if left == right else left - right
+        case int(), list():
+            left = [left]
+        case list(), int():
+            right = [right]
+    if left == [] or right == []:
+        return 0 if len(left) == len(right) else len(left) - len(right)
+
+    result = comp_pair(left[0], right[0])
+    if result != 0:
+        return result
+
+    return comp_pair(left[1:], right[1:])
+
+
+in_order = sorted(decoded, key=cmp_to_key(comp_pair))
+
+decoder_key = (in_order.index([[2]]) + 1) * (in_order.index([[6]]) + 1)
+print(decoder_key)
